1546d73ba8e176dfd050bead1be98c625d1dcb2d
palakg01 <palakgupta6506@gmail.com>
2021-07-24 21:03:51 +0530

    commit

--- src/components/Challenge.jsx
+++ src/components/Challenge.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import Test from "./Test";
-import Typewriter from "typewriter-effect";
+// import Typewriter from "typewriter-effect";
+import React from "react";
 
 const Challenge = ({
   timeLeft,
